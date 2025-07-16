95d5927fd948159b16af523572cf5ff924b1b0c9
hubert <hboksz@gmail.com>
2025-07-15 22:31:20 -0400

    working sddm installer

--- install-sddm-theme.sh
+++ install-sddm-theme.sh
@@ -21,22 +21,22 @@ echo "Editing SDDM conf..."
 echo "[Theme]
 Current=sddm-astronaut-theme" | sudo tee /etc/sddm.conf
 
-echo "Editing vitrualkbd..."
-echo "[General]
-InputMethod=qtvirtualkeyboard" | sudo tee /etc/sddm.conf.d/virtualkbd.conf
+#echo "Editing vitrualkbd..."
+#echo "[General]
+#InputMethod=qtvirtualkeyboard" | sudo tee /etc/sddm.conf.d/virtualkbd.conf
 
 # TODO: check if dir exists
 echo "Copy over background image..."
 sudo cp ~/dotfiles/backgrounds/fern.jpg /usr/share/sddm/themes/sddm-astronaut-theme/Backgrounds/
 
 echo "Copy theme..."
-sudo cp /usr/share/sddm/theme/sddm-astronaut-theme/Themes/astronaut.conf /usr/share/sddm/theme/sddm-astronaut-theme/Themes/fern.conf 
+sudo cp /usr/share/sddm/themes/sddm-astronaut-theme/Themes/astronaut.conf /usr/share/sddm/themes/sddm-astronaut-theme/Themes/fern.conf 
 
 echo "Replace image in new theme..."
-sudo sed -i 's|Background="[^"]*"|Background="Backgrounds/fern.jpg"|' /usr/share/sddm/theme/sddm-astronaut-theme/Themes/fern.conf 
+sudo sed -i 's|^Background="Backgrounds/[^"]*"|Background="Backgrounds/fern.jpg"|' /usr/share/sddm/themes/sddm-astronaut-theme/Themes/fern.conf 
 
 echo "Set new theme as default..."
-sudo sed -i 's|ConfigFile=Themes/[^ ]*|ConfigFile=Themes/fern.conf|' /usr/share/sddm/theme/sddm-astronaut-theme/metadata.desktop
+sudo sed -i 's|^ConfigFile=Themes/[^ ]*|ConfigFile=Themes/fern.conf|' /usr/share/sddm/themes/sddm-astronaut-theme/metadata.desktop
 
 echo "Done."
 
